52aa2868b04a47e67bbf939bd891ee661038bf90
Antoine Niek <antoine.niek@adgear.com>
2017-03-11 12:13:49 -0500

    Fix style

--- ruby/lib/slugrs.rb
+++ ruby/lib/slugrs.rb
@@ -4,17 +4,22 @@ require 'ffi'
 module Slugrs
   module Binding
     extend FFI::Library
-    ffi_lib File.join(
+
+    LIB_PATH = File.join(
       File.dirname(__FILE__),
-      "../ext/target/release/libslug_ffi.#{FFI::Platform::LIBSUFFIX}"
+      '../ext/target/release',
+      "libslug_ffi.#{FFI::Platform::LIBSUFFIX}"
     )
-    attach_function :generate, :slug_generate, [:string], :strptr
-    attach_function :free, :slug_free, [:pointer], :void
+
+    ffi_lib(LIB_PATH)
+    attach_function(:generate, :slug_generate, [:string], :strptr)
+    attach_function(:free, :slug_free, [:pointer], :void)
   end
 
   def self.slugify(str)
     slug, ptr = Binding.generate(str)
     FFI::AutoPointer.new(ptr, Binding.method(:free))
+
     slug
   end
 end
