776f12dc1fcc2ca171ee48eeef5e1f88b85bbb73
Antoine Niek <antoine.niek@adgear.com>
2017-03-11 11:36:54 -0500

    Return input if null

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,12 @@
 extern crate slug;
 extern crate libc;
 
-use std::ptr;
 use libc::c_char;
 use std::ffi::{CStr, CString};
 
 #[no_mangle]
-pub extern fn slugify(input: *const c_char) -> *mut c_char {
-    if input.is_null() {
-        return ptr::null_mut();
-    }
+pub extern fn slugify(input: *const c_char) -> *const c_char {
+    if input.is_null() { return input; }
 
     let c_str = unsafe { CStr::from_ptr(input) };
     let input = c_str.to_str().unwrap();
