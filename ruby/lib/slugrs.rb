4baa0f5a1345ede2a71f06451074a590257d2d02
Antoine Niek <antoine.niek@adgear.com>
2017-03-11 12:01:26 -0500

    Free rust allocated slug with autopointer

--- ruby/lib/slugrs.rb
+++ ruby/lib/slugrs.rb
@@ -2,10 +2,19 @@ require 'slugrs/version'
 require 'ffi'
 
 module Slugrs
-  extend FFI::Library
-  ffi_lib File.join(
-    File.dirname(__FILE__),
-    "../ext/target/release/libslug_ffi.#{FFI::Platform::LIBSUFFIX}"
-  )
-  attach_function :slugify, [:string], :string
+  module Binding
+    extend FFI::Library
+    ffi_lib File.join(
+      File.dirname(__FILE__),
+      "../ext/target/release/libslug_ffi.#{FFI::Platform::LIBSUFFIX}"
+    )
+    attach_function :generate, :slug_generate, [:string], :strptr
+    attach_function :free, :slug_free, [:pointer], :void
+  end
+
+  def self.slugify(str)
+    slug, ptr = Binding.generate(str)
+    FFI::AutoPointer.new(ptr, Binding.method(:free))
+    slug
+  end
 end
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,25 @@
 extern crate slug;
 extern crate libc;
 
+use std::ptr;
 use libc::c_char;
 use std::ffi::{CStr, CString};
 
 #[no_mangle]
-pub extern fn slugify(input: *const c_char) -> *const c_char {
-    if input.is_null() { return input; }
+pub extern fn slug_generate(input: *const c_char) -> *mut c_char {
+    if input.is_null() { return ptr::null_mut(); }
 
     let c_str = unsafe { CStr::from_ptr(input) };
     let input = c_str.to_str().unwrap();
     let output = slug::slugify(input);
     CString::new(output).unwrap().into_raw()
+}
+
+#[no_mangle]
+pub extern fn slug_free(s: *mut c_char) {
+    if s.is_null() { return }
+
+    unsafe {
+        CString::from_raw(s)
+    };
 }
